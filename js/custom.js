a035d1e14dab85ee3a5608fa2006240b99294d89
cristiano-nicolau <cristianonicolau@ua.pt>
2024-07-30 19:45:53 +0100

    dev-update all website

---
+++ js/custom.js
@@ -0,0 +1,177 @@
+(function ($) {
+
+  "use strict";
+
+  // Function to initialize particles
+  function initParticles() {
+    const isDarkMode = $('body').hasClass('dark-mode');
+    const particleColor = isDarkMode ? '#f1f1f1' : '#000000';
+
+    particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value": 400,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": particleColor
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 10,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 500,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 2
+        },
+        "move": {
+          "enable": true,
+          "speed": 6,
+          "direction": "bottom",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "bubble"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 0.5
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 4,
+            "duration": 0.3,
+            "opacity": 1,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 200,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+    });
+  }
+
+  // Initialize particles on page load
+  initParticles();
+
+  // COLOR MODE
+  $('.color-mode').click(function(){
+    $('.color-mode-icon').toggleClass('active');
+    $('body').toggleClass('dark-mode');
+    $('#particles-js').html(''); // Clear existing particles
+    initParticles(); // Reinitialize particles with new color scheme
+  });
+
+  // HEADER
+  $(".navbar").headroom();
+
+
+  $(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 3, // Exibir 1 "slide" por vez
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1, // Exibir 1 item por vez em dispositivos móveis
+                margin: 5
+            },
+            768: {
+                items: 2 // Exibir 1 item por vez em tablets
+            },
+            992: {
+                items: 3 // Exibir 1 item por vez em desktops
+            }
+        }
+    });
+});
+
+
+  // SMOOTHSCROLL
+  $(function() {
+    $('.nav-link, .custom-btn-link').on('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+
+  // TOOLTIP
+  $('.social-links a').tooltip();
+
+})(jQuery);
