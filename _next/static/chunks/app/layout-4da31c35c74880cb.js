2071b40aa329f6b04311e00f860619df85db50d5
cristiano-nicolau <cristiano-nicolau@users.noreply.github.com>
2025-10-30 20:17:52 +0000

    deploy: d89e4adf8a56d5c127f8cdda00b75b792daa5590

---
+++ _next/static/chunks/app/layout-4da31c35c74880cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{8863:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9754,23))},9754:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[241,230,569,358],()=>s(8863)),_N_E=e.O()}]);
